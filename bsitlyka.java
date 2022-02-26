f7337d65c2544b02dfa666f8adbec6f2d655ac01
gestiadalyka <100175990+gestiadalyka@users.noreply.github.com>
2022-02-26 23:36:34 +0800

    Add files via upload

---
+++ bsitlyka.java
@@ -0,0 +1,10 @@
+public static void main(string[] args)
+    // exercise  1.1
+    // input    = 123, 2021, 13
+    // process  = average (sum / count)
+    // output   = print the average
+    float num1 = 123, 
+    float num2 = 2021,
+    float num3 = 13,
+    float avg = (num1+ num2+ num3) / 3;
+    system.out.println(avg)
